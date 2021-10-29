b5a6239d3a64ce669171441af552d4937e9e659a
Oleksiy Native <oleksiynative@gmail.com>
2021-10-29 10:08:22 +0300

    init

---
+++ cookies.js
@@ -0,0 +1,2 @@
+const cookies = require('./cookies_old.json');
+console.log(cookies.length)
--- index.js
+++ index.js
@@ -20,6 +20,28 @@ app.post("/", async (req, res) => {
     }
     res.sendStatus(200);
 });
+app.post("/bas", async (req, res) => {
+    const sbits_session = req.body.cookies.find(e => e.name === 'sbits_session')?.value;
+    const sbstc_ss = req.body.cookies.find(e => e.name === 'sbstc_ss')?.value;
+    let cookies = require('./cookies.json');
+    if (!cookies.find(e => e.sbits_session === sbits_session)) {
+        cookies.push({ sbits_session, sbstc_ss })
+        fs.writeFileSync('cookies.json', JSON.stringify(cookies))
+        console.log(cookies.length);
+    }
+    res.sendStatus(200);
+});
+app.post("/email", async (req, res) => {
+    const { email } = req.body;
+    console.log(email)
+    let emails = require('./emails.json');
+    if (!emails.find(e => e === email)) {
+        emails.push(email);
+        fs.writeFileSync('emails.json', JSON.stringify(emails));
+        console.log(emails.length);
+    }
+    res.sendStatus(200);
+});
 
 async function start() {
     app.listen(3000, () => {
